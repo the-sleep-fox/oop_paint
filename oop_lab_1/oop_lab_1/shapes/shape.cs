ff6981a53c9f2a73d24b639575631b740905f0e8
Alesia_Petushok <petusokalesa@gmail.com>
2025-03-14 11:59:19 +0300

    Added functional folder shapes, abstract class shape ( with methods erase and draw for all derrivated classes) and class circle ( without test for now)

---
+++ oop_lab_1/oop_lab_1/shapes/shape.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_lab_1.shapes
+{
+    abstract class shape
+    {
+        public int CenterX { get; set; }
+        public int CenterY { get; set; }
+        public char Symbol { get; set; }
+
+        public abstract void Draw(char[,] canvas);
+        public abstract void Erase(char[,] canvas);
+    }
+}
